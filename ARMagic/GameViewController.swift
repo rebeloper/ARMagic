d32a9a485a500834d0ac5664719c5a568b609c95
Alex <therebeloper@gmail.com>
2018-01-13 12:55:38 +0200

    reset scene

--- ARMagic/GameViewController.swift
+++ ARMagic/GameViewController.swift
@@ -34,7 +34,7 @@ class GameViewController: UIViewController {
   
   @objc func handlePlusButtonTapped() {
     print("Tapped on plus button")
-    addBox()
+    addNode()
   }
   
   let minusButtonWidth = ScreenSize.width * 0.1
@@ -53,6 +53,24 @@ class GameViewController: UIViewController {
   @objc func handleMinusButtonTapped() {
     print("Tapped on minus button")
     removeAllBoxes()
+  }
+  
+  let resetButtonWidth = ScreenSize.width * 0.1
+  lazy var resetButton: UIButton = {
+    var button = UIButton(type: .system)
+    button.setImage(#imageLiteral(resourceName: "ResetButton").withRenderingMode(.alwaysTemplate), for: .normal)
+    button.tintColor = UIColor(white: 1.0, alpha: 0.7)
+    button.layer.cornerRadius = resetButtonWidth * 0.5
+    button.layer.masksToBounds = true
+    button.addTarget(self, action: #selector(handleResetButtonTapped), for: .touchUpInside)
+    button.layer.zPosition = 1
+    button.imageView?.contentMode = .scaleAspectFill
+    return button
+  }()
+  
+  @objc func handleResetButtonTapped() {
+    print("Tapped on reset button")
+    resetScene()
   }
   
   let configuration = ARWorldTrackingConfiguration()
@@ -87,24 +105,42 @@ class GameViewController: UIViewController {
     plusButton.anchor(nil, left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: nil, topConstant: 0, leftConstant: 24, bottomConstant: 12, rightConstant: 0, widthConstant: plusButtonWidth, heightConstant: plusButtonWidth)
     
     view.addSubview(minusButton)
-    minusButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 12, rightConstant: 24, widthConstant: plusButtonWidth, heightConstant: plusButtonWidth)
+    minusButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 12, rightConstant: 24, widthConstant: minusButtonWidth, heightConstant: minusButtonWidth)
+    
+    view.addSubview(resetButton)
+    resetButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 12, rightConstant: 0, widthConstant: resetButtonWidth, heightConstant: resetButtonWidth)
+    resetButton.anchorCenterXToSuperview()
   }
   
-  func addBox() {
-    let boxNode = SCNNode()
-    boxNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0002)
-    boxNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Material")
-    boxNode.position = SCNVector3(Float.random(-0.5, max: 0.5), Float.random(-0.5, max: 0.5), Float.random(-0.5, max: 0.5))
-    boxNode.name = "box"
-    arView.scene.rootNode.addChildNode(boxNode)
+  func addNode() {
+    let shapeNode = SCNNode()
+//    boxNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0002)
+//    shapeNode.geometry = SCNCapsule(capRadius: 0.05, height: 0.20)
+//    shapeNode.geometry = SCNCone(topRadius: 0.05, bottomRadius: 0.10, height: 0.15)
+//    shapeNode.geometry = SCNTorus(ringRadius: 0.10, pipeRadius: 0.02)
+    shapeNode.geometry = SCNTube(innerRadius: 0.10, outerRadius: 0.20, height: 0.15)
+    shapeNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Material")
+    shapeNode.position = SCNVector3(0.0, 0.0, -0.3)
+    shapeNode.name = "node"
+    arView.scene.rootNode.addChildNode(shapeNode)
   }
   
   func removeAllBoxes() {
     arView.scene.rootNode.enumerateChildNodes { (node, _) in
-      if node.name == "box" {
+      if node.name == "node" {
         node.removeFromParentNode()
       }
     }
+  }
+  
+  func resetScene() {
+    arView.session.pause()
+    arView.scene.rootNode.enumerateChildNodes { (node, _) in
+      if node.name == "node" {
+        node.removeFromParentNode()
+      }
+    }
+    arView.session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
   }
   
   
